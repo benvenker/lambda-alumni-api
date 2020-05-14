3dbad87a478e1eaf9b1984bc519f6ff83be547b2
Ben Venker <bvemails@gmail.com>
2020-05-14 09:46:36 -0500

    add cors

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 
 const server = express();
 const port = process.env.PORT;
 server.use(express.json());
+server.use(cors());
 
 module.exports = {
   server,
