e47093e83b980b8701daf2b2d9744a85e867e13f
Ben Venker <bvemails@gmai.com>
2020-07-15 14:37:11 -0500

    try some things to get the db connected

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,16 @@
 module.exports = {
-  development: {
-    client: "pg",
+  production: {
+    client: 'pg',
     connection: {
       host: process.env.DB_URL,
       user: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       port: process.env.DB_PORT,
+    },
+    pool: {
+      max: 20,
+      min: 5,
     },
   },
 };
