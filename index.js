a26348fa365cf375d932f19645cbd08603b58f96
Ben Venker <bvemails@gmai.com>
2020-07-15 11:32:18 -0500

    migrates votes routes to their own routes file

--- index.js
+++ index.js
@@ -14,26 +14,6 @@ server.get('/', (req, res) => {
   res.send('<h1>Welcome to the Lambda Alumni API');
 });
 
-// TODO: Migrate to a votesRouter.js file? Create votes model and router files?
-server.post('/votes', (req, res) => {
-  return votes.getVotes(req.body.post_id).then(v => res.json(v));
-});
-
-// TODO: Change to a GET, accept query params, and add to votesRouter.js
-server.post('/check-vote', (req, res) => {
-  return votes
-    .checkUserVote(req.body)
-    .then(vote => {
-      console.log(vote);
-      vote === undefined
-        ? res.status(404).json({ message: 'No vote found' })
-        : res.status(200).json(vote);
-    })
-    .catch(err =>
-      res.status(500).json({ message: 'Failed to get vote, server error.' })
-    );
-});
-
 server.get('/knex', (req, res) => {
   return comments.testKnexOutput().then(output => res.json(output));
 });
--- votes/votesRouter.js
+++ votes/votesRouter.js
@@ -3,4 +3,25 @@ const votes = require('./votesDb');
 const checkJwt = require('../middleware/checkJwt');
 const router = express.Router();
 
+server.get('/:id', (req, res) => {
+  return votes.getVotes(req.params.id).then(v => res.json(v));
+});
+
+// Check to see if a vote exists
+server.get('/check-vote', (req, res) => {
+  const { username, post_id } = req.query;
+  const user = { useranme: username, post_id: post_id };
+  return votes
+    .checkUserVote(user)
+    .then(vote => {
+      console.log(vote);
+      vote === undefined
+        ? res.status(404).json({ message: 'No vote found' })
+        : res.status(200).json(vote);
+    })
+    .catch(err =>
+      res.status(500).json({ message: 'Failed to get vote, server error.' })
+    );
+});
+
 module.exports = router;
