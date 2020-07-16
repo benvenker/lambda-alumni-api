94afb3357ea79a93edbb2ac5558517804aeebaa6
Ben Venker <bvemails@gmai.com>
2020-07-16 10:04:04 -0500

    remove unnecessary endpoint

--- votes/votesRouter.js
+++ votes/votesRouter.js
@@ -3,19 +3,7 @@ const votes = require('./votesDb');
 const checkJwt = require('../middleware/checkJwt');
 const router = express.Router();
 
-router.get('/id/:postId', (req, res) => {
-  if (!req.params.id) {
-    return res
-      .status(404)
-      .json({ error: 'Vote not found. Are you sure you included an ID?' });
-  }
-  return votes
-    .getVotes(req.params.id)
-    .then(v => res.stats(200).json({ message: v }))
-    .catch(err => res.status(500).json({ error: err }));
-});
-
-// Check to see if a vote exists
+// Check to see if a vote exists / a user has already voted on the post
 router.get('/check-vote', (req, res) => {
   const { username, post_id } = req.query;
   const user = { username: username, post_id: post_id };
