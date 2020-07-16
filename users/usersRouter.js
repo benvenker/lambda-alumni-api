e4e5bfe0b2e7db87596367f695c40bc6619ea901
Ben Venker <bvemails@gmai.com>
2020-07-16 09:40:09 -0500

    improve usersRouter error handling

--- users/usersRouter.js
+++ users/usersRouter.js
@@ -9,14 +9,16 @@ router.get('/username', (req, res) => {
 });
 
 router.post('/', (req, res) => {
+  if (!req.body.username || req.body.username === undefined) {
+    return res.status(404).json({ error: 'Username is required.' });
+  }
   if (req.body.username !== '') {
     return users.addUser(req.body.username).then(user => {
-      res.json(user);
+      res.status(200).json(user);
     });
   } else if (req.body.username === '') {
     return null;
   }
-
   return req.body;
 });
 
