f35513b4ae19f536b3a7eee502454463a9d85ec4
Ben Venker <bvemails@gmai.com>
2020-07-16 09:41:02 -0500

    fix up votesRouter

--- votes/votesRouter.js
+++ votes/votesRouter.js
@@ -3,14 +3,22 @@ const votes = require('./votesDb');
 const checkJwt = require('../middleware/checkJwt');
 const router = express.Router();
 
-router.get('/:id', (req, res) => {
-  return votes.getVotes(req.params.id).then(v => res.json(v));
+router.get('/id/:postId', (req, res) => {
+  if (!req.params.id) {
+    return res
+      .status(404)
+      .json({ error: 'Vote not found. Are you sure you included an ID?' });
+  }
+  return votes
+    .getVotes(req.params.id)
+    .then(v => res.stats(200).json({ message: v }))
+    .catch(err => res.status(500).json({ error: err }));
 });
 
 // Check to see if a vote exists
 router.get('/check-vote', (req, res) => {
   const { username, post_id } = req.query;
-  const user = { useranme: username, post_id: post_id };
+  const user = { username: username, post_id: post_id };
   return votes
     .checkUserVote(user)
     .then(vote => {
@@ -22,6 +30,14 @@ router.get('/check-vote', (req, res) => {
     .catch(err =>
       res.status(500).json({ message: 'Failed to get vote, server error.' })
     );
+});
+
+// Upvote a post
+router.post('/upvote', (req, res) => {
+  return votes
+    .upVote(req.body)
+    .then(vote => res.json(vote))
+    .catch(err => res.status(500).json({ error: err }));
 });
 
 module.exports = router;
